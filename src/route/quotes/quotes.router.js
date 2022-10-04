516450d2fbd7e381b2e81fa9f6a90754653371bb
Morgen Diaz <morgen@webslinger.works>
2022-10-04 09:03:33 -1000

    Setup relationship between main api route and quotes endpoint.

---
+++ src/route/quotes/quotes.router.js
@@ -0,0 +1,5 @@
+const express = require(`express`);
+
+const quotesRouter = express.Router();
+
+module.exports = quotesRouter;
--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 const express = require(`express`);
 const morgan = require(`morgan`);
+const api = require(`./route/api`);
 
 const { PORT = 4000 } = process.env;
 
 const server = express();
 
 server.use(morgan("combined"));
+server.use(`/api`, api);
 
 server.listen(PORT, () => {
   console.log("its quite good in the hood on the block of port ", PORT);
