d35f3fd1b3a65e84229ea1e02b70591719fa1696
Morgen Diaz <morgen@webslinger.works>
2022-10-04 14:14:18 -1000

    Added raw test data for quotes and tags.

---
+++ src/database/quotes.testdata.js
@@ -0,0 +1,57 @@
+const quotes = [
+  {
+    id: 1,
+    content: `Men who can both be right and sit tight are uncommon`,
+    author: `Jesse Livermore`,
+    date: `June 9, 2019`,
+    tags: [`Day-Trading`, `Stock Market`],
+  },
+  {
+    id: 2,
+    content: `I did not want to be consoled, I wanted my rage to be validated, justified.`,
+    author: `Robin Hobb`,
+    date: `January 4, 2020`,
+    tags: [`Personal`],
+  },
+  {
+    id: 3,
+    content: `Tomorrow owes us nothing, but the sum of our yesterdays. Some people hope desperately that the debt is never paid in full.`,
+    author: `Robin Hobb`,
+    date: `January 1, 2020`,
+    tags: [`Motivation`, `Life`],
+  },
+
+  {
+    id: 4,
+    content: `The real power of human intelligence is not the collection of information, but the connections of information.`,
+    author: `David Butler`,
+    date: `January 4, 2020`,
+    tags: [`Intelligence`, `Self-Development`, `Thinking`, `Creativity`],
+  },
+
+  {
+    id: 5,
+    content: `When we speak without jargon, it frees us from hiding behind knowledge we don't have.`,
+    author: `Richard Feynman`,
+    date: `January 14, 2020`,
+    tags: [`Intelligence`, `Thinking`, `Learning`],
+  },
+
+  {
+    id: 6,
+    content: `Predicting rain doesn't count. Building arks does.`,
+    author: `Warren Buffet`,
+    date: `January 14, 2020`,
+    tags: [`Self-Development`, `Discipline`, `Life`],
+  },
+
+  {
+    id: 7,
+    content: `I don't make threats, Im just telling you what i'm going to do.`,
+    author: `Martha Wells`,
+    date: `January 14, 2020`,
+    tags: [`Personal`, `Funny`, `Aspergers`],
+  },
+];
+
+module.exports = quotes;
---
+++ src/database/tags.testData.js
@@ -0,0 +1,17 @@
+const tags = [
+  `Day-Trading`,
+  `Stock Market`,
+  `Personal`,
+  `Motivation`,
+  `Life`,
+  `Intelligence`,
+  `Self-Development`,
+  `Thinking`,
+  `Creativity`,
+  `Learning`,
+  `Discipline`,
+  `Funny`,
+  `Aspergers`,
+];
+
+module.exports = tags;
